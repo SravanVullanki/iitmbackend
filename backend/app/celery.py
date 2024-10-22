8418de89f915545e5902eafc856bb5f07cd145a5
SravanVullanki <85739869+SravanVullanki@users.noreply.github.com>
2024-10-22 11:45:07 +0530

    Delete backend directory

--- backend/app/celery.py
+++
@@ -1,6 +0,0 @@
-from celery import Celery
-
-def make_celery(app):
-    celery = Celery(app.import_name, broker='redis://localhost:6379/0')  # Adjust the Redis URL if needed
-    celery.conf.update(app.config)
-    return celery
